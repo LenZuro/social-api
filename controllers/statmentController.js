aedacc5c0302e0ecc312690dd54f83112a929b72
LenZuro <126126939+LenZuro@users.noreply.github.com>
2023-08-10 13:35:40 -0700

    statment contoller created

--- controllers/statmentController.js
+++ controllers/statmentController.js
@@ -56,6 +56,55 @@ const statmentController = {
        }
     },
 
-    
+    async deleteStatment(req , res) {
+        try {
+            const statment = await Statment.findOneAndDelete({
+                _id: req.params.statmentId,
+            });
+            if (!statment){
+                return res.status(404).json({message: "No statment with this Id",});
+            }
+            return res.status(200).json({message: "Statment and reactions successfully deleted",});
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err)
+        }
+    },
 
-}
+    async addReaction(req ,res){
+        try{
+            const reaction = await Statment.findOneAndUpdate(
+                { _id: req.params.statmentId },
+                { $addToSet: { reactions:req.body }},
+                { runValidators: true },
+            );
+            if(!reaction){
+                return res.status(404).json({message: "No statment with this Id",});
+            }
+            return res.status(200).json(reaction);
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
+
+    async deleteRaction( req , res ){
+        try{
+            const reaction = await Statment.findOneAndUpdate(
+                { _id: req.params.statmentId },
+                { $pull: { reactions: { _id: req.params.reactionId} } },
+                { runValidators: true , new: true },
+            );
+            if(!reaction){
+                return res.status(404).json({message: "Please check the statment and reaction Id "});
+            }
+            return res.status(200).json(reaction);
+        } catch(err){
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
+
+};
+
+module.exports = statmentController;
